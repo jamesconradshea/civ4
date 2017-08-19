2cbfcb6cbc001cf82d12652be98e683b8023d693
jamesconradshea <jamesconradshea@gmail.com>
2017-08-18 19:51:57 -0500

    Fix bug in file update time on Windows 10

---
+++ btsColorEditor/btsColorEditorConfig.py
@@ -0,0 +1,18 @@
+#!python
+
+# Copyright 2010-2017 by James Conrad Shea (duckstab)
+# This software is made available under the terms of the Creative Commons 
+# Attribution-NonCommercial-ShareAlike 2.0 License
+# See http://creativecommons.org/licenses/by-nc-sa/2.0/legalcode for full terms 
+
+# CustomAssets folder to which color editor will save files; it is created if it does not exist.
+# The color editor also searches this folder at startup for existing civ and color info
+
+COLOR_EDITOR_CUSTOM_ASSETS="c:/jim/My Games/Beyond the Sword/CustomAssets"
+
+# Path to search for existing assets -- this is a list of directories separated by semicolons. The color editor
+# will look here for any files it needs that aren't found under the target custom asset folder above
+
+COLOR_EDITOR_ASSET_PATH="c:/Program Files (x86)/2K Games/Firaxis Games/Sid Meier's Civilization 4 Gold/Beyond the Sword/Assets;c:/Program Files (x86)/2K Games/Firaxis Games/Sid Meier's Civilization 4 Gold/Assets"
+
+
